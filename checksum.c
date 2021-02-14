881532864d1f41bb8c3744f9b145fff8c401d3c7
LyanRarson <earthradar@aol.com>
2021-02-14 14:16:35 -0800

    working 8-bit checksum

--- checksum.c
+++ checksum.c
@@ -4,7 +4,8 @@
 /* Date: 2/14/21                */
 /********************************/
 /* Description:                 */
-/* Validation Checks:           */
+/* Validation Checks: must be   */
+/*     passed at least 10 bytes */
 /* Enhancements:                */
 /********************************/
 
@@ -13,7 +14,9 @@
 #include <unistd.h>
 
 #define max_int (255)
-#define byte (char)
+#define byte unsigned char
+
+#define checksum_index 5
 
 int main (int argc, char * argv[], char ** envp) {
 
@@ -21,19 +24,32 @@ int main (int argc, char * argv[], char ** envp) {
 	int sum = 0;   
 	byte checksum; 
 	byte complement;
-	byte *buffer = malloc(sizeof(byte[10])); // allocate buffer
+	byte *buffer = malloc(sizeof(byte[count])); // allocate buffer
 
-	if(read(int fildes, void *buf, size_t nbyte) != 10) {
-		fprintf(stdout, "Error: input must equal 10 bytes!\n");
+	if(read(0, (void*)buffer, count) < count) {
+		fprintf(stderr, "Error: input must be at least %i bytes!\n", count);
 		return 2;
 	}
 
-	for(int i = 0; i < 10; i++) {
-		
+	for(int i = 0; i < count; i++) {
+		if(i == checksum_index) {
+			checksum = buffer[i];
+			buffer[i] = 0;
+		}
+		else {
+			sum += buffer[i];
+			if(sum > max_int)
+			{
+				sum = sum % (max_int + 1);
+				sum++; // will always carry 1
+			}
+		}
 	}
+	
+	complement = max_int - sum;
 
-	fprintf(stdout, "Stored Checksum: %d, Computed Checksum: %d\n", checksum, complement);
-	if (checksum != complement ) {
+	fprintf(stdout, "Stored Checksum: %u, Computed Checksum: %u\n", checksum, complement);
+	if (checksum != complement) {
 		fprintf(stderr, "Error Detected!\n"); 
 		return 1;
 	}
