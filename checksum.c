7bdd059cac0d71b8fcc953c308be148bbf7ce146
LyanRarson <earthradar@aol.com>
2021-02-14 10:49:05 -0800

    Create checksum.c w/starter code

---
+++ checksum.c
@@ -0,0 +1,34 @@
+/********************************/
+/* Program Name:                */
+/* Author:                      */
+/* Date:                        */
+/********************************/
+/* Description:                 */
+/* Validation Checks:           */
+/* Enhancements:                */
+/********************************/
+
+#include "stdio.h"
+#include "stdlib.h"
+#include <unistd.h>
+
+#define max_int (255)
+#define byte (char)
+
+int main (int argc, char * argv[], char ** envp) {
+
+  int count = 10;
+  int sum = 0;   
+  byte checksum; 
+  byte complement;
+
+  /* the following is the prototype for the read system call */
+  /* int read(int fildes, void *buf, size_t nbyte);  */
+
+  fprintf(stdout, "Stored Checksum: %d, Computed Checksum: %d\n", checksum, complement);
+  if (checksum != complement ) {
+    fprintf(stderr, "Error Detected!\n"); 
+    return 1;
+  }
+  return 0;
+}
